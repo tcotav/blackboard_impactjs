a09bab68ecb868eb143441b96d8df1dfa54322c9
james <tcotav@gmail.com>
2011-10-13 16:57:57 -0700

    added dynamic framing and dynamic text flow to entity
    Such that the blackboard/billboard background is no longer a graphic
    but rather drawn directly on the canvas.  Similarly, the text can be
    input in one big clump and the javascript will flow it into billboard.
    Still makes some assumptions about layout, but that is easily fixed by
    tweaking source.

--- entities/billboard.js
+++ entities/billboard.js
@@ -5,7 +5,7 @@ Entity in center of screen
 Keys for Weltmeister:
 
 name -- used by trigger to invoke
-text.1, text.2, text.3 -- text to be written to billboard
+text -- written to billboard
 
 */
 
@@ -17,35 +17,115 @@ ig.module(
 )
 .defines(function(){
     EntityBillboard = ig.Entity.extend({
-        size: {x: 150, y: 64},
+        size: {x: 32, y: 32},
         name: "billboard",
-        textlist:[],
 
-		animSheet: new ig.AnimationSheet("media/blackboard.png", 150, 64),
+        // size of rectangle
+        /* 
+            bit of philosophical debate here -- we could make this dynamic 
+            such that it changed with text.  I argue that aethetically, we're
+            better served with a fixed size.  Dynamic though would just be
+            some tweaks to drawing below:  finding area of text first, then 
+            drawing rectangles based on that, then drawing text */
+        rectHeight: 100,
+        rectWidth: 150,
+
+        // difference between white and black frames
+        billboardFrame:5,
+        text:"",
+        textFooter:"press [enter] to continue",
+        fontString:"12px Courier",
+        // extra padding between edge of text and border of rect
+        textPadding: 5,
+
+        // space between lines of text
+        textLeading: 20, 
+
         init: function(x,y,settings) {
 			// Add animations for the animation sheet from
             // Weltmeister settings
             if(settings.text) {
-                this.textlist = settings.text;
+                this.text = settings.text;
             }
-			this.addAnim("blank", 1, [0]);
             this.parent(x,y,settings);
         },
-       draw: function(){
-            this.parent();
-            var offset=0;
-            var ddrawX = this.pos.x - ig.game.screen.x + this.size.x/2;
-            var ddrawY = this.pos.y - ig.game.screen.y + 10;
-            for (x in this.textlist) {
-                ig.game.font.draw( this.textlist[x], 
-                    ddrawX, ddrawY+offset, 
-                    ig.Font.ALIGN.CENTER );
-                offset=offset+10;
+        _drawText: function(ctx) {
+            // break text string up into words
+            var lwords = this.text.split(" ");
+
+            ctx.font = this.fontString;
+            
+            var currentLine="";
+            var linecount=0;
+            var lineList=[]
+
+            var maxwidth = this.rectWidth - (this.textPadding *2);
+            var maxheight = this.rectHeight - (this.textPadding * 2);
+       
+            // tick through list of words
+            for(x=0; x<lwords.length; x++) {
+                // if line + newest word < maxw, then append and continue
+                if (ctx.measureText(currentLine + " "+ lwords[x]).width <= maxwidth) {
+                    currentLine = currentLine + " " + lwords[x];
+                }
+                else { // past end of row -- save off and create new row
+                    linecount+=1;
+                    lineList.push(currentLine);
+                    // what if lwords is greater than width????
+                    currentLine = lwords[x];
+                }
+                if(x == lwords.length-1) {
+                    lineList.push(currentLine);
+                    break;
+                }
             }
-            // hardcoded for now/simplicity
-            ig.game.font.draw( "[space] to continue",
-                ddrawX, ddrawY+offset + 15, 
-                ig.Font.ALIGN.CENTER );
+
+            if(this.textFooter) {
+                lineList.push("");
+                lineList.push(this.textFooter);
+            }
+
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+
+            widthDraw=ig.system.realWidth/2;
+            ctx.fillStyle="white";
+
+            // need to center text vertically
+            totalTextHeight = this.textPadding * 2 
+                + this.textLeading * (lineList.length-1);
+            startDraw = (ig.system.realHeight - totalTextHeight)/2;
+            
+            for(var x=0; x< lineList.length; x++) {
+                ctx.fillText(lineList[x], widthDraw, startDraw + this.textLeading*x);
+            }
+       },
+       draw: function(){
+           // no call to this.parent() -- we're doing all the drawing here
+            var ctx = ig.system.context;
+           
+            var wwidth=this.rectWidth * ig.system.scale; 
+            var hheight=this.rectHeight * ig.system.scale;
+
+            var offsetX = ig.system.realWidth/2-wwidth/2;
+            var offsetY = ig.system.realHeight/2-hheight/2;
+
+            // first draw the white frame
+            ctx.fillStyle="white";
+            ctx.fillRect(
+                offsetX, 
+                offsetY,
+                wwidth, hheight);
+
+            // then plop the black drawing area inside of that
+            ctx.fillStyle="black";
+            ctx.fillRect(
+                offsetX + this.billboardFrame, 
+                offsetY + this.billboardFrame, 
+                wwidth-(this.billboardFrame * 2), 
+                hheight-(this.billboardFrame * 2));
+
+            this._drawText(ctx);
 
             // flip this boolean in game -- this pauses all activity
             ig.game.fBillboardPause = true;
